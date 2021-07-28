b00fab9e575359d1e6c7616be010fe49f01313ff
Catalina Williams <webdevcatalina@gmail.com>
2021-07-28 11:39:04 -0600

    added mongoDB connection string, model, schema, and .env

--- server.js
+++ server.js
@@ -5,18 +5,33 @@ const cors = require("cors");
 const path = require("path");
 const port = process.env.PORT || 5000;
 
+require('dotenv').config()
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 
 app.use(cors());
 
-//mongoose
+//MONGODB ATLAS CONNECTION STRING
+mongoose.connect(MONGODB_URI);
+
+//MONGOOSE SCHEMA
+const projectSchema = {
+    title: String,
+    description: String,
+    technologies: Array,
+    heroku: String,
+    github: String,
+}
+
+//SETTING MONGOOSE MODEL
+const Project = mongoose.model("Project", projectSchema);
 
 //API routes
 
 //test to see if server is running
-app.get('/', function(req, res) {
-    res.send("express is here")
+app.get('/projects', function(req, res) {
+    Project.find().then(projects => res.json(projects));
 })
 
 app.listen(port, function() {
