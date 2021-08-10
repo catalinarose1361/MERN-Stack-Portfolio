42a275886ea43386f4f81f92a5cccf48f2f4860f
Catalina Williams <webdevcatalina@gmail.com>
2021-08-10 14:43:51 -0600

    added working links to sidebar

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,36 +1,44 @@
 import React from 'react';
 
-import  { Sidebar, Avatar, Nav } from 'grommet';
+import  { Sidebar, Avatar, Nav, ResponsiveContext, Anchor, Menu } from 'grommet';
 
-import { DownloadOption, Linkedin, StackOverflow, Github, Mail } from 'grommet-icons';
+import { DownloadOption, Linkedin, StackOverflow, Github, Mail, ContactInfo } from 'grommet-icons';
 
 import avatarPic from '../images/IMG1.png';
 
+import resume from "../documents/Catalina's Resume.pdf"
+
+import certification from '../documents/certificate.pdf'
 function SideBar () {
 
     return (
 
+      
         <Sidebar gap="large" height="min: '100%'">
 
             <Nav>
 
                 <Avatar src={avatarPic} />
 
-                <Linkedin />
+                <Anchor icon={ <Linkedin size="large" /> } href="https://www.linkedin.com/in/catalinarwilliams/" />
 
-                <Github />
+                <Anchor icon={ <StackOverflow size="large" /> } href="https://stackoverflow.com/users/11933391/catalina"  />
 
-                <Mail />
+                <Anchor icon={ <Github size="large"/> } href="https://github.com/catalinarose1361"  />
 
-                <StackOverflow />
-                
-                <DownloadOption />
+                <Anchor icon={ <Mail size="large" /> } href="mailto:webdevcatalina@gmail.com" />
+                {/* DOWNLOAD RESUME */}
+                {/* ADD "download" TO END OF ANCHOR TAG TO MAKE DOCUMENT DOWNLOADABLE */}
+                <Anchor icon={ <DownloadOption size="large" /> } href={resume} />
+                {/* DOWNLOAD CERTIFICATION */}
+                <Anchor icon={ <DownloadOption size="large" /> } href={certification} />
+
+                {/* DIGITAL BUSINESS CARD */}
+                <Anchor icon={ <ContactInfo size="large" /> } href="https://mycrd.is/catalina"/>
 
             </Nav>
-       
 
         </Sidebar>
-
     )
 
 }
