a93620589c32dc928c47220ab5e6f56eb94df5ea
Catalina Williams <webdevcatalina@gmail.com>
2021-08-30 12:58:58 -0600

    readied application for deployment to heroku

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ app.get('/projects', function(req, res) {
 
 })
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 app.listen(PORT, function() {
 
     console.log("express is running");
